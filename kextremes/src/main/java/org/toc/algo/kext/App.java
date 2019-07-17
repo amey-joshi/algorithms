14db39e52bd8536e062d0780e75cddbdf3045893
Amey Joshi <amey.s.joshi@acm.org>
2019-07-17 21:43:14 +0530

    SonarLint suggestions.

--- kextremes/src/main/java/org/toc/algo/kext/App.java
+++ kextremes/src/main/java/org/toc/algo/kext/App.java
@@ -1,6 +1,9 @@
 package org.toc.algo.kext;
 
 import java.util.Arrays;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
@@ -8,15 +11,21 @@ import java.util.stream.Collectors;
  *
  */
 public class App {
+	private static Logger logger = Logger.getLogger(App.class.getName());
+
 	public static void main(String[] args) {
 		KExtreme kmin = new KExtreme(5);
-		int[] elements = {16, 21, 43, 40, 15, 6, 75, 81, 9, 10}; 
+		int[] elements = { 16, 21, 43, 40, 15, 6, 75, 81, 9, 10 };
 		show(kmin.getLeastElements(elements));
 		show(kmin.getMostElements(elements));
 	}
-	
+
 	private static void show(int[] extremes) {
 		String s = Arrays.stream(extremes).mapToObj(String::valueOf).collect(Collectors.joining(","));
-		System.err.println(s);
+		logger.setLevel(Level.ALL);
+		ConsoleHandler consoleHandler = new ConsoleHandler();
+		consoleHandler.setLevel(Level.ALL);
+		logger.addHandler(consoleHandler);
+		logger.fine(s);
 	}
 }
