2a2c9a2aac0106d245de2bb1d50fb2342f5e9d03
Amey Joshi <amey.s.joshi@acm.org>
2020-02-13 21:55:02 +0530

    Formatting alone.

--- strings/src/main/java/org/toc/algo/string/SubSeqTester.java
+++ strings/src/main/java/org/toc/algo/string/SubSeqTester.java
@@ -2,12 +2,12 @@ package org.toc.algo.string;
 
 public class SubSeqTester {
 
-	public static void main(String[] args) {
-		SubsequenceAnalyzer ssa = new SubsequenceAnalyzer("xyz");
-		String[] examples = { "xyt", "xyzxy", "xyzxzyx" };
+    public static void main(String[] args) {
+        SubsequenceAnalyzer ssa = new SubsequenceAnalyzer("xyz");
+        String[] examples = { "xyt", "xyzxy", "xyzxzyx" };
 
-		for (String s : examples) {
-			System.out.println(String.format("%s has %d sub-sequences of %s", s, ssa.numSubsequences(s), ssa.inits()));
-		}
-	}
+        for (String s : examples) {
+            System.out.println(String.format("%s has %d sub-sequences of %s", s, ssa.numSubsequences(s), ssa.inits()));
+        }
+    }
 }
--- strings/src/main/java/org/toc/algo/string/SubsequenceAnalyzer.java
+++ strings/src/main/java/org/toc/algo/string/SubsequenceAnalyzer.java
@@ -1,89 +1,89 @@
 package org.toc.algo.string;
 
 public class SubsequenceAnalyzer {
-	private String initial;
+    private String initial;
 
-	public SubsequenceAnalyzer(final String initial) {
-		this.initial = initial;
-	}
+    public SubsequenceAnalyzer(final String initial) {
+        this.initial = initial;
+    }
 
-	public String inits() {
-		return initial;
-	}
+    public String inits() {
+        return initial;
+    }
 
-	/**
-	 * Count the number of subsequences of 'initial' that make up the 'goal'. If
-	 * none are possible, return zero.
-	 * 
-	 * @param goal
-	 * @return
-	 */
-	public int numSubsequences(final String goal) {
-		int nss = -1; // number of sub-sequences
+    /**
+     * Count the number of subsequences of 'initial' that make up the 'goal'. If
+     * none are possible, return zero.
+     * 
+     * @param goal
+     * @return
+     */
+    public int numSubsequences(final String goal) {
+        int nss = -1; // number of sub-sequences
 
-		if (isSubset(goal)) {
-			nss = 0;
-			int from = 0;
-			int to = 1;
-			for (int i = to; i <= goal.length(); ++i) {
+        if (isSubset(goal)) {
+            nss = 0;
+            int from = 0;
+            int to = 1;
+            for (int i = to; i <= goal.length(); ++i) {
 
-				if (isSubSeqPossible(goal.substring(from, to))) {
-					++to;
-				} else {
-					from = to - 1;
-					--i;
-					++nss;
-				}
-			}
-			++nss;
-		}
+                if (isSubSeqPossible(goal.substring(from, to))) {
+                    ++to;
+                } else {
+                    from = to - 1;
+                    --i;
+                    ++nss;
+                }
+            }
+            ++nss;
+        }
 
-		return nss;
-	}
+        return nss;
+    }
 
-	/**
-	 * Can you generate 'goal' from the characters in 'initial'?
-	 * 
-	 * @param initial
-	 * @param goal
-	 * @return
-	 */
-	private boolean isSubset(String goal) {
-		boolean possible = true;
+    /**
+     * Can you generate 'goal' from the characters in 'initial'?
+     * 
+     * @param initial
+     * @param goal
+     * @return
+     */
+    private boolean isSubset(String goal) {
+        boolean possible = true;
 
-		for (char g : goal.toCharArray()) {
-			if (initial.indexOf(g) == -1) {
-				possible = false;
-				break;
-			}
-		}
+        for (char g : goal.toCharArray()) {
+            if (initial.indexOf(g) == -1) {
+                possible = false;
+                break;
+            }
+        }
 
-		return possible;
-	}
+        return possible;
+    }
 
-	/**
-	 * Can a part of the 'goal' be a sub-sequence of 'initial'?
-	 * 
-	 * @param partOfGoal
-	 * @return
-	 */
-	private boolean isSubSeqPossible(String partOfGoal) {
-		boolean possible = true;
-		int[] positions = new int[partOfGoal.length()];
+    /**
+     * Can a part of the 'goal' be a sub-sequence of 'initial'?
+     * 
+     * @param partOfGoal
+     * @return
+     */
+    private boolean isSubSeqPossible(String partOfGoal) {
+        boolean possible = true;
+        int[] positions = new int[partOfGoal.length()];
 
-		int from = 0;
-		for (int i = 0; i < partOfGoal.length(); ++i) {
-			int pos = initial.indexOf(partOfGoal.charAt(i), from);
+        int from = 0;
+        for (int i = 0; i < partOfGoal.length(); ++i) {
+            int pos = initial.indexOf(partOfGoal.charAt(i), from);
 
-			if (pos != -1) {
-				positions[i] = pos;
-				from = pos;
-			} else {
-				possible = false;
-				break;
-			}
-		}
+            if (pos != -1) {
+                positions[i] = pos;
+                from = pos;
+            } else {
+                possible = false;
+                break;
+            }
+        }
 
-		return possible;
-	}
+        return possible;
+    }
 }
