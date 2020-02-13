928e90dd19d924c525785585dcd3c666bf7d636c
Amey Joshi <amey.s.joshi@acm.org>
2020-02-13 21:19:11 +0530

    Initial commit.

---
+++ strings/src/main/java/org/toc/algo/string/SubSeqTester.java
@@ -0,0 +1,13 @@
+package org.toc.algo.string;
+
+public class SubSeqTester {
+
+	public static void main(String[] args) {
+		SubsequenceAnalyzer ssa = new SubsequenceAnalyzer("xyz");
+		String[] examples = { "xyt", "xyzxy", "xyzxzyx" };
+
+		for (String s : examples) {
+			System.out.println(String.format("%s has %d sub-sequences of %s", s, ssa.numSubsequences(s), ssa.inits()));
+		}
+	}
+}
